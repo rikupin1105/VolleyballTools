91dddb69134ef7c2d54b54e256ea93fd9add4e7f
kaminaka rikuya <rikupin1105@gmail.com>
2023-06-14 23:46:44 +0900

    TESTを追加

---
+++ src/VolleyballToolsTests/PDF/PDFTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VolleyballTools.PDF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolleyballTools.PDF.Tests
+{
+    [TestClass()]
+    public class PDFTests
+    {
+        [TestMethod()]
+        public void Generate5SETTest()
+        {
+            var pdf = new PDF();
+
+            var stream = pdf.Generate5SET("試合名","開催地名","会場名",DateTime.Now,"A-12","ATeam","BTeam",true,DateTime.Now);
+            
+            
+            Assert.Fail();
+        }
+    }
+}
