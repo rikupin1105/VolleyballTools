4971d8733efa02f80c5f79406069b8cdfd998f2f
kaminaka rikuya <rikupin1105@gmail.com>
2023-07-26 17:17:17 +0900

    model

---
+++ src/VolleyballTools/Model/Match.cs
@@ -0,0 +1,15 @@
+﻿namespace VolleyballTools.Model
+{
+    public class Match : Competition
+    {
+        /// <summary>
+        /// 性別
+        /// </summary>
+        public Sex? Sex { get; set; }
+
+        /// <summary>
+        /// 試合番号
+        /// </summary>
+        public string? MatchNumber { get; set; }
+    }
+}
