23c6998cfeafa7f7f091041b7fb7d7532263d645
kaminaka rikuya <rikupin1105@gmail.com>
2023-07-28 18:34:30 +0900

    クラス変更

--- src/VolleyballTools/Model/Match.cs
+++ src/VolleyballTools/Model/Match.cs
@@ -11,5 +11,13 @@
         /// 試合番号
         /// </summary>
         public string? MatchNumber { get; set; }
+
+        public string? ATeamName { get; set; }
+        public string? BTeamName { get; set; }
+    }
+    public class NineParsonMatch : Match
+    {
+        public List<Player> ATeamPlayers { get; set; } = new();
+        public List<Player> BTeamPlayers { get; set; } = new();
     }
 }
